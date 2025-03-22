3d0611a8339bf203692cd1912062eb7d9027e12d
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-03-22 16:02:17 +0800

    Update main.sh

--- image-classification-master/vision_transformer/main.sh
+++ image-classification-master/vision_transformer/main.sh
@@ -1,4 +1,1 @@
-source /home/yjzhang/anaconda3/etc/profile.d/conda.sh
-conda activate pytorch1
-
-python main.py --device cuda:5 --root /datasets_hdd2/yjzhang/yajie/video_fujian --batch_size 8 --n_frame 100 --lr 0.0001 --epoch 100
+python main.py --device cuda:5 --root yourpath --batch_size 8 --n_frame 100 --lr 0.0001 --epoch 100
