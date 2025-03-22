7c3c43367809ce4ce48ca5749897b331d8da0329
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-03-22 16:01:05 +0800

    Update main.sh

--- image-classification-master/Test8_densenet/main.sh
+++ image-classification-master/Test8_densenet/main.sh
@@ -1,4 +1,1 @@
-source /home/yjzhang/anaconda3/etc/profile.d/conda.sh
-conda activate pytorch1
-
 python main.py --device cuda:7 --root yourpath --batch_size 64 --n_frame 100 --lr 0.0001 --epoch 100
