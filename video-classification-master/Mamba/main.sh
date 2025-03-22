6561c27c9965f35d820dbc975fa435fb2e66c8fd
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-03-22 16:03:42 +0800

    Update main.sh

--- video-classification-master/Mamba/main.sh
+++ video-classification-master/Mamba/main.sh
@@ -1,1 +1,1 @@
-python main.py --device cuda:6 --root /nfs/yajie/video_fujian --batch_size 8 --n_frame 50  --epoch 100
+python main.py --device cuda:6 --root yourpath --batch_size 8 --n_frame 50  --epoch 100
