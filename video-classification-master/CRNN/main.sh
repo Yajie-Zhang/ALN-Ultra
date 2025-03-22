de299ea8670728a1f04e716fc25aee7e5d687b8a
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-03-22 16:03:07 +0800

    Update main.sh

--- video-classification-master/CRNN/main.sh
+++ video-classification-master/CRNN/main.sh
@@ -1,4 +1,1 @@
-source /home/yjzhang/anaconda3/etc/profile.d/conda.sh
-conda activate pytorch1
-
-python main.py --device cuda:5 --root /nfs/yajie/video_fujian --batch_size 8 --n_frame 50 --epoch 100
+python main.py --device cuda:5 --root yourpath --batch_size 8 --n_frame 50 --epoch 100
