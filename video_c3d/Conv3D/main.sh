4592794960b04b1133dfd02dcd9ba1226986092c
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-03-22 16:05:34 +0800

    Update main.sh

--- video_c3d/Conv3D/main.sh
+++ video_c3d/Conv3D/main.sh
@@ -1,4 +1,1 @@
-source /home/yjzhang/anaconda3/etc/profile.d/conda.sh
-conda activate pytorch1
-
-python main.py --device cuda:6 --root /nfs/yajie/video_fujian --batch_size 8 --n_frame 50 --lr 0.0001 --epoch 100
+python main.py --device cuda:6 --root yourpath --batch_size 8 --n_frame 50 --lr 0.0001 --epoch 100
