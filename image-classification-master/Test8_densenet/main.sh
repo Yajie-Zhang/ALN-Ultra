4dc9749a7c2d15030b8b105ab13d83dfe47cfb7c
Yajie-Zhang <85613090+Yajie-Zhang@users.noreply.github.com>
2025-12-23 20:59:43 +0800

    Delete image-classification-master directory

--- image-classification-master/Test8_densenet/main.sh
+++
@@ -1,1 +0,0 @@
-python main.py --device cuda:7 --root yourpath --batch_size 64 --n_frame 100 --lr 0.0001 --epoch 100
--- image-classification-master/Test9_efficientNet/main.sh
+++
@@ -1,1 +0,0 @@
-python main.py --device cuda:5 --root yourpath --batch_size 8 --n_frame 100 --lr 0.0001 --epoch 100
